b16f4314dea153d3a5403a44929eb7e847ba24e8
YudiMatheus <matheus.yudi@helpper.com.br>
2021-04-07 21:18:52 -0300

    refactor: Implementing validation on bigger.js

--- Atividade7/bigger.js
+++ Atividade7/bigger.js
@@ -1,6 +1,13 @@
 let getBigger = (numbers) => numbers.sort((x, y) => x - y).pop();
 
-// 1;2;3; -> ['1', '2', '3'] -> [1, 2, 3]
-const values = prompt('Digite 3 números separados por ;').split(';').map(item => Number(item));
+let numbers = [];
 
-alert('The bigger is: ' + getBigger(values));
+while (numbers.length < 3) {
+    const value = Number(prompt('Type a number:'));
+
+    if (!isNaN(value)) {
+        numbers.push(value);
+    }
+}
+
+alert('The bigger is: ' + getBigger(numbers));
